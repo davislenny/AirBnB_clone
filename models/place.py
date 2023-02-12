acafe6fc19aa434dfb340d54c4f69336ccd734da
davislenny <davidlmwangi7@gmail.com>
2023-02-12 09:44:42 -0800

    Add place.py

---
+++ models/place.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+The 'place' module
+Defines the class 'Place'
+"""
+from models.base_module import BaseModel
+
+
+class Place(BaseModel):
+    """ Class definition """
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guests = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
