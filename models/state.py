de9507013e31c2da485adaff03508afb994c5baa
davislenny <davidlmwangi7@gmail.com>
2023-02-12 09:44:03 -0800

    Add state.py

---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+The 'state' module
+Defines class 'State'
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """ Class definition """
+    name = ""
