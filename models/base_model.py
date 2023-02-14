0689b5a4e3ace67738b52f9d1ada8ff6a9acb190
davislenny <davidlmwangi7@gmail.com>
2023-02-14 00:06:12 -0800

    Add base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -15,18 +15,20 @@ class BaseModel:
         if kwargs is not None and kwargs != {}:
             for key in kwargs:
                 if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
+                    self.created_at = datetime.strptime(
                         kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(
+                    self.updated_at = datetime.strptime(
                         kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == '__class__':
+                    pass
                 else:
                     self.__dict__[key] = kwargs[key]
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+#            models.storage.new(self)
 
     def __str__(self):
         """ Returns string representation of a class """
@@ -39,8 +41,8 @@ class BaseModel:
         'updated_at' with the current datetime
         """
         self.updated_at = datetime.now()
+        models.storage.new(self)
         models.storage.save()
-        pass
 
     def to_dict(self):
         """
@@ -49,9 +51,6 @@ class BaseModel:
         """
         _dict = self.__dict__.copy()
         _dict["__class__"] = self.__class__.__name__
-        for k, v in self.__dict__.items():
-            if isinstance(v, (datetime, )):
-                _dict[k] = v.isoformat()
-            else:
-                _dict[k] = v
+        _dict["created_at"] = _dict["created_at"].isoformat()
+        _dict["updated_at"] = _dict["updated_at"].isoformat()
         return _dict
